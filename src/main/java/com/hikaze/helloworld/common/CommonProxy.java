798f4930982fe54e991dca55337ddd2e916d40d9
Hikaze <Hikaze@DESKTOP-A4JM84H>
2018-06-02 02:23:53 +0800

    A green modder met a magic problem.

--- src/main/java/com/hikaze/helloworld/common/CommonProxy.java
+++ src/main/java/com/hikaze/helloworld/common/CommonProxy.java
@@ -3,12 +3,12 @@ package com.hikaze.helloworld.common;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
+import com.hikaze.helloworld.items.*;
 public class CommonProxy
 {
     public void preInit(FMLPreInitializationEvent event)
     {
-
+    	new ItemLoader(event);
     }
 
     public void init(FMLInitializationEvent event)
